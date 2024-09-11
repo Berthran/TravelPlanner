35451aef3258a5d283c4f489303b5c61b67c81dd
kofiadaba <kofiadaba@gmail.com>
2024-09-11 14:51:52 +0000

    install dependencies and create page structure

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+// client/src/components/Navbar.jsx
+
+import '../styles/navbar.scss'
+
+import {
+    faBars
+} from '@fortawesome/free-solid-svg-icons'
+import {
+    FontAwesomeIcon
+} from "@fortawesome/react-fontawesome";
+import {
+    Link
+} from "react-router-dom"
+
+
+
+const Navbar = () => {
+
+    return (
+        <div className='navContainer'>
+            <Link to="/">
+                <p className='navLogo'>JourneyDash</p>
+            </Link>
+
+            <input type="checkbox" id='menu-bar' />
+            <label htmlFor="menu-bar">
+                <FontAwesomeIcon icon={faBars}
+                    className="icon" />
+            </label>
+            <nav className='navbar'>
+                <ul>
+                    <Link to="/">
+                        <li><p>Home</p></li>
+                    </Link>
+                    <Link to="/dashboard">
+                        <li><p>Dashboard</p></li>
+                    </Link>
+                    <Link to="/login">
+                        <li><p>Login</p></li>
+                    </Link>
+                    <Link to="/register">
+                        <li><p>Register</p></li>
+                    </Link>
+                </ul>
+            </nav>
+        </div >
+    )
+}
+
+export default Navbar
