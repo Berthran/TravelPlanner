06367107c5955b71be54e3d6966e0503a1615cd8
kofiad <kofiadaba@gmail.com>
2024-09-15 17:36:55 +0000

    Ignore backend/migrations/db_prep.sql in gitignore

--- backend/migrations/db_prep.sql
+++ backend/migrations/db_prep.sql
@@ -1,5 +1,5 @@
 -- For preparing database i.e for localhost storage(mysql)
-CREATE USER IF NOT EXISTS 'traveller'@'localhost' IDENTIFIED BY 'traveller';
+CREATE USER IF NOT EXISTS 'traveller'@'localhost' IDENTIFIED BY 'Trave@ller47ss';
 CREATE DATABASE IF NOT EXISTS travel_planner;
 GRANT ALL PRIVILEGES ON travel_planner.* TO 'traveller'@'localhost';
 FLUSH PRIVILEGES;
