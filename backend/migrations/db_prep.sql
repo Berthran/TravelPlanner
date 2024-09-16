8b7f8972417ce3d54de3234731f3789fcea2e8ce
kofiad <kofiadaba@gmail.com>
2024-09-16 15:35:23 +0000

    Refactor .gitignore to include backend/migrations/db_prep.sql and backend

--- backend/migrations/db_prep.sql
+++
@@ -1,6 +0,0 @@
--- For preparing database i.e for localhost storage(mysql)
-CREATE USER IF NOT EXISTS 'traveller'@'localhost' IDENTIFIED BY 'traveller';
-CREATE DATABASE IF NOT EXISTS travel_planner;
-GRANT ALL PRIVILEGES ON travel_planner.* TO 'traveller'@'localhost';
-FLUSH PRIVILEGES;
-SHOW GRANTS FOR 'traveller'@'localhost';
--- backend/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-from api.v1.utils.ai import generate_city_keyword
-
-print(generate_city_keyword( "Lagos"))
-
-
