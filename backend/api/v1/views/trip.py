8610b93b76dd085d23a1201d9e6b5c7bda82c153
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-09-16 19:33:04 +0100

    Updated the trip.py

--- backend/api/v1/views/trip.py
+++ backend/api/v1/views/trip.py
@@ -16,6 +16,7 @@ from flask_jwt_extended import jwt_required
 
 
 @app_views.route("/place", methods=["POST"], strict_slashes=False)
+@jwt_required()
 def get_weather_condition():
     """Get weather condition of a place from city_name
 
